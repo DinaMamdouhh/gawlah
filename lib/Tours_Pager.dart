c6bebe79df3d285d3f87ef9122195afd73eb5aa3
DinaMamdouhh <dina.mahfouz@outlook.com>
2020-02-20 12:53:00 +0200

    hiiii

--- lib/Tours_Pager.dart
+++ lib/Tours_Pager.dart
@@ -1,10 +1,13 @@
 import 'dart:collection';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_googlemaps/main.dart';
+import 'map.dart';
 
 class Tours_List extends StatefulWidget {
+  final String size;
+  const Tours_List({Key key, this.size}) : super(key: key);
   createState() => TourListState();
 }
 
@@ -12,13 +15,10 @@ class TourListState extends State<Tours_List> {
   // This will give them 80% width which will allow other slides to appear on the side
   final PageController controller = PageController(viewportFraction: .8);
   final Firestore database = Firestore.instance;
-
   Stream tours;
-
   Color Back;
   String activeTag = 'all';
   int currentPage = 0;
-
   TourListState();
 
   Container BuildThemesPage(BuildContext context, List themes) {
@@ -26,7 +26,6 @@ class TourListState extends State<Tours_List> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
-
         children: [
           Text(
             'Themes',
@@ -37,13 +36,17 @@ class TourListState extends State<Tours_List> {
             height: 10,
           ),
           Row(
-children: <Widget>[
-  Container(width: 16,
-  )
-  ,   Text('FILTER', style: TextStyle(color: Colors.black26) ,textAlign: TextAlign.center,),
-],
+            children: <Widget>[
+              Container(
+                width: 16,
+              ),
+              Text(
+                'FILTER',
+                style: TextStyle(color: Colors.black26),
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
-
           _buildThemesButton(themes),
         ],
       ),
@@ -58,48 +61,56 @@ children: <Widget>[
     final double top = active ? 200 : 240;
 
     return GestureDetector(
+      
         child: AnimatedContainer(
-          duration: Duration(milliseconds: 500),
-          curve: Curves.easeOutQuint,
-          margin: EdgeInsets.only(top: top, bottom: 100, right: 10, left: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(snap.data.documents[index]['image']),
-
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeOutQuint,
+      margin: EdgeInsets.only(top: top, bottom: 100, right: 10, left: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(snap.data.documents[index]['image']),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black87,
+            blurRadius: blur,
+            offset: Offset(offset, offset),
+          ),
+        ],
+      ),
+      
+    
+      child: Column(
+        children: [
+          Container(
+            height: 100,
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              snap.data.documents[index]['name'],
+              style: TextStyle(fontSize: 30, color: Colors.white),
+              textAlign: TextAlign.left,
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black87,
-                blurRadius: blur,
-                offset: Offset(offset, offset),
-              ),
-            ],
           ),
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-              ),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  snap.data.documents[index]['name'],
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                  textAlign: TextAlign.left,
-                ),
-              ),
-            ],
-          ),
-        ));
+        ],
+      ),
+    ),
+   onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Myhi(
+             
+            ))
+        );
+      },
+    );
   }
 
   @override
   void initState() {
-
-
-
     Back = Color.fromRGBO(225, 186, 107, 1);
     queryDatabase();
     controller.addListener(() {
@@ -113,15 +124,15 @@ children: <Widget>[
     super.initState();
   }
 
-  void queryDatabase({String tag = 'favourites'}) {
+  void queryDatabase({String theme = 'favourites'}) {
     Query query =
-        database.collection('tours').where('themes', arrayContains: tag);
+        database.collection('tours').where('themes', arrayContains: theme);
     // Map the slides to the data payload
     tours =
         query.snapshots().map((list) => list.documents.map((doc) => doc.data));
     // Update the active tag
     setState(() {
-      activeTag = tag;
+      activeTag = theme;
     });
   }
 
@@ -131,49 +142,50 @@ children: <Widget>[
     List<Widget> themes_list = new List<Widget>();
     for (var i = 0; i < themes.length; i++) {
       Color color = themes[i] == activeTag ? Colors.blue : Colors.transparent;
-      themes_list.add( FlatButton(
+      themes_list.add(FlatButton(
           color: color,
-
           child: SizedBox(
-              width: 60,
-             child :Text(
-
-           '#'+ themes[i],
-            style: TextStyle(),
-            textAlign: TextAlign.left,
-          ),),
+            width: 60,
+            child: Text(
+              '#' + themes[i],
+              style: TextStyle(),
+              textAlign: TextAlign.left,
+            ),
+          ),
           onPressed: () {
-            queryDatabase(tag: themes[i]);
-
+           queryDatabase(theme: themes[i]);
             activeTag = themes[i];
-          }));
+
+           
+          }
+          ));
     }
-    return new Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-       children: themes_list);
+    return new 
+     Column(
+        crossAxisAlignment: CrossAxisAlignment.start, 
+        children: themes_list);
+        
+        
   }
-
-
-
-
 
   Widget build(
     BuildContext context,
-  ) {return
-       MaterialApp(
-                home: Scaffold(
-                  body: Stack(children: [
-      BuildBackground(),
-      StreamBuilder<QuerySnapshot>(
+  ) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(children: [
+          BuildBackground(),
+          StreamBuilder<QuerySnapshot>(
               stream: database.collection('tours').snapshots(),
               builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                 return PageView.builder(
                     controller: controller,
                     onPageChanged: _onPageViewChange,
-                    itemCount: snapshot.data.documents.length+1,
+                    itemCount: snapshot.data.documents.length + 1,
                     itemBuilder: (context, int currentIndex) {
                       if (currentIndex == 0) {
-                        return BuildThemesPage(context, themes_getter(snapshot));
+                        return BuildThemesPage(
+                            context, themes_getter(snapshot));
                       } else if ((snapshot.data.documents.length + 1) >=
                           currentIndex) {
                         bool active = currentIndex == currentPage;
@@ -182,22 +194,14 @@ children: <Widget>[
                       }
                     });
               })
-    ]),
-         ),
-       );
-
+        ]),
+      ),
+    );
   }
 
-
-
-
-
   Widget BuildBackground() {
-    return Scaffold
-      (
-      body :
-          Container
-          (
+    return Scaffold(
+        body: Container(
       color: Back,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -215,7 +219,6 @@ children: <Widget>[
     ));
   }
 
-
   List themes_getter(AsyncSnapshot snapshot) {
     List themes = new List();
 
@@ -231,8 +234,5 @@ children: <Widget>[
     return themes;
   }
 
-
-  void _onPageViewChange(int page) {
-
-  }
+  void _onPageViewChange(int page) {}
 }
---
+++ lib/map.dart
@@ -0,0 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'Tours_Pager.dart';
+
+class Myhi extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<Myhi> {
+ 
+bool isLoading = false;
+String errorMessage;
+//final Set<Polyline> polyline={};
+//List<LatLng>routecoords;
+//GoogleMapPolyline googleMapPolyline=new GoogleMapPolyline(apiKey: "AIzaSyBPDbF9SG2qPGN_nS57lSYhXmdnR-ksx04");
+
+
+  GoogleMapController _mapcontroller;
+  static const _intialPositionn = LatLng(29.9565261, 31.2703018);
+  LatLng _lastMapPosition = _intialPositionn;
+  final Set<Marker> _markers = {};
+
+  MapType _currentMapType = MapType.normal;
+
+  void _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+
+
+ 
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        
+        infoWindow: InfoWindow(
+          title: 'Really cool place',
+          snippet: '5 Star Rating',
+        ),
+          icon: BitmapDescriptor.fromAsset('assets/emoji.png')
+
+      ));
+    });
+  }
+
+  searchandNavigate() {
+    Geolocator().placemarkFromAddress(searchAddr).then((result) {
+      _mapcontroller.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: LatLng(
+                  result[0].position.latitude, result[0].position.longitude),
+              zoom: 10.0)));
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  void _onMapCreated(controller) async {
+    setState(() {
+      _mapcontroller = controller;
+   
+
+    });
+  }
+
+  String searchAddr;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            StreamBuilder<Object>(
+              stream: Firestore.instance.collection('tours').snapshots(),
+              builder: (context, AsyncSnapshot snapshot) {
+                return GoogleMap(
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(29.9565261, 31.2703018),
+                    zoom: 10.0,
+                  ),);},),
+          ],
+                ),
+            )
+        );
+  }
+}
